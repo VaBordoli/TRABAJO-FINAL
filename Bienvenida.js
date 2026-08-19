175eee81110f0291b4ca76fe5e4b80858323a01b
VaBordoli <valentino.bordoli@gmail.com>
2026-08-19 10:38:02 -0300

    TERMINEEE

---
+++ Bienvenida.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const botonDerecho = document.querySelector(".boton-derecho");
+    const campoTexto = document.querySelector(".campo-texto");
+  
+    // Ejemplo opcional: si querés evitar que navegue si el input está vacío
+    // botonDerecho.closest("a").addEventListener("click", function (e) {
+    //   if (campoTexto.value.trim() === "") {
+    //     e.preventDefault();
+    //     alert("Escribí algo antes de crear la cuenta");
+    //   }
+    // });
+  
+  });
+  
+  
